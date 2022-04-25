64b440d6479c28a92e325a06ac93a8b4965219cc
Tangilur Rahman <mohammadtangilurrahman@gmail.com>
2022-04-25 14:08:46 +0600

    for submit project on heroku

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 const model = require("./model/model");
 
@@ -10,27 +11,35 @@ app.use(express.json());
 app.post("/user", async (req, res) => {
 	const { name, email, message } = req.body;
 
-   
-
 	try {
-
-		if(name && email){
+		if (name && email) {
 			const document = await model({ name, email, message });
 
-		await document.save();
+			await document.save();
 
-		res.status(200).json({ "message": "Sended Successfully 💚" });
-		}else{
-			res.status(500).json({ "error": "Fill-Up Properly 🙏" });
+			res.status(200).json({ message: "Sended Successfully 💚" });
+		} else {
+			res.status(500).json({ error: "Fill-Up Properly 🙏" });
 		}
-		
 	} catch (error) {
-		
-		res.status(500).json({ "error": "Invalid Email! 👽"});
+		res.status(500).json({ error: "Invalid Email! 👽" });
 	}
 });
 
-const PORT = process.env.PORT;
+// submit on heroku
+if (process.env.NODE_ENV == "production") {
+	app.use(express.static("build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname + "/build/index.html"));
+	});
+} else {
+	app.get("/", (req, res) => {
+		res.send("client disconnected");
+	});
+}
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
 	console.log(`server was running at http://localhost:${PORT}`);
